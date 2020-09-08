645e75548a9c7a3ae935638c8e815163c40ec2ae
zhangzheng <851103999@qq.com>
2020-09-08 20:44:54 +0800

    immutable.js 通过这个库防止修改state

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -37,7 +37,7 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        focused: state.header.get('focused')
     }
 
 }
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,21 @@
 import * as actionTypes from './constants'
+import {fromJS} from 'immutable'//把state变成不可修改对象
 
-const defaultState = {
-    focused:false
-}
+const defaultState = fromJS({
+    focused: false
+})
 
-export default (state = defaultState,action)=>{
-    if(action.type === actionTypes.SEATCH_FOCUS){
-        return{
-            focused: true
-        }
+export default (state = defaultState, action) => {
+    if (action.type === actionTypes.SEATCH_FOCUS) {
+        return state.set('focused', true) // 通过这种方法来"写入" set focused
+        // immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个全新的对象
+        // return{
+        //    focused:true
+        // }
     }
 
-    if(action.type === actionTypes.SEARCH_BLUR){
-        return{
-            focused: false
-        }
+    if (action.type === actionTypes.SEARCH_BLUR) {
+        return state.set('focused', false)
     }
     return state
 }
